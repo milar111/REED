5d44395eac1aa641719c33db0e714e3674102919
milar111 <114105495+milar111@users.noreply.github.com>
2025-02-21 23:14:05 +0200

    test

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.Node_ENV === "production";
 const nextConfig = {
   output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: isProd ? "/REED" : "", 
-  assetPrefix: isProd ? "/REED/" : "",
 };
 
 module.exports = nextConfig;
