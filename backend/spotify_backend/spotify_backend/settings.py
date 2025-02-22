32691ab9e6eeb4d7ae3a50c6fda50d1d0ca1dbc0
milar111 <114105495+milar111@users.noreply.github.com>
2025-02-22 13:20:55 +0200

    settings update allowed hosts

--- backend/spotify_backend/spotify_backend/settings.py
+++ backend/spotify_backend/spotify_backend/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l4=&s8*d)d$65r=5hfaqxd9+oagiy-p@ft)p%wbnv-4+g@77ww'
+SECRET_KEY = os.getenv("SECURITY_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'reed-ocsk.onrender.com']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
