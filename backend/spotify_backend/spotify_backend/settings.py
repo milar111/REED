01524fe2d53971297bbb4a826f6022134f5fbb68
milar111 <114105495+milar111@users.noreply.github.com>
2025-03-15 00:46:21 +0200

    test

--- backend/spotify_backend/spotify_backend/settings.py
+++ backend/spotify_backend/spotify_backend/settings.py
@@ -131,7 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Default primary key field type
